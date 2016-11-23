c228d40dc357d9b50ccc034fee5e95d969b7d429
Adam Sypniewski <ajsyp@syptech.net>
2016-11-23 10:46:38 -0500

    Added Source classes, which are responsible for actually handling an individual data stream.

---
+++ kur/sources/source.py
@@ -0,0 +1,70 @@
+"""
+Copyright 2016 Deepgram
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+################################################################################
+class Source:
+	""" Base class for all data sources.
+
+		Data sources are responsible for producing data that will be presented
+		to the model for training, validation, or prediction (evaluation).
+
+		Each data source provides one of the input or output tensors that the
+		network needs.
+	"""
+
+	############################################################################
+	def __init__(self):
+		""" Create a new data source.
+		"""
+		pass
+
+	############################################################################
+	def __iter__(self):
+		""" Return an iterator to the data.
+
+			The iterator should return a tensor of shape `(X, ) + self.shape()`,
+			where `X` is the number of entries provided by this data source.
+		"""
+		raise NotImplementedError
+
+	############################################################################
+	def __len__(self):
+		""" Returns the total number of entries that this source can return, if
+			known.
+
+			# Return value
+
+			If the total number of entries that this source can return is known,
+			it should be returned. In the simplest case, this is the same as the
+			number of training samples in an epoch. If this number is unknown or
+			effectively infinite (because it is from some real-time data
+			generator or is being streamed from some uncontrolled source, for
+			example), then this should return None.
+		"""
+		raise NotImplementedError
+
+	############################################################################
+	def shape(self):
+		""" Return the shape of the tensor (excluding batch size) returned by
+			this data source.
+
+			# Return value
+
+			A shape tuple (length of each dimension in the tensor).
+		"""
+		raise NotImplementedError
+
+#### EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF
---
+++ kur/sources/vanilla.py
@@ -0,0 +1,84 @@
+"""
+Copyright 2016 Deepgram
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from . import Source, Shuffleable
+
+################################################################################
+class VanillaSource(Source, Shuffleable):
+	""" A data source which is simple wrapper around an in-memory array.
+	"""
+
+	############################################################################
+	def __init__(self, data, chunk_size=None, *args, **kwargs):
+		""" Create a new vanilla data provider.
+
+			# Arguments
+
+			data: numpy-like array. The data to provide.
+			chunk_size: int (default: None). The maximum number of samples to
+				return at each iteration, or None to return all samples.
+
+			# Notes
+
+			- This class will not "take ownership" of the dataset; that is, it
+			  only holds a reference to the data. This means that you are free
+			  to shuffle the underlying dataset (or even change the shape or
+			  length of the dataset, although this isn't necessarily a good
+			  idea).
+		"""
+		super().__init__(*args, **kwargs)
+
+		self.data = data
+		self.chunk_size = chunk_size
+
+	############################################################################
+	def __len__(self):
+		""" Returns the number of samples this source provides.
+		"""
+		return self.data.shape[0]
+
+	############################################################################
+	def shape(self):
+		""" Returns the shape of the individual data samples.
+		"""
+		return self.data.shape[1:]
+
+	############################################################################
+	def shuffle(self, indices):
+		""" Applies a permutation to the data.
+		"""
+		if len(indices) > len(self):
+			raise ValueError('Shuffleable was asked to apply permutation, but '
+				'the permutation is longer than the length of the data set.')
+		self.data[:len(indices)] = self.data[indices]
+
+	############################################################################
+	def __iter__(self):
+		""" Returns the next chunk of data.
+		"""
+
+		start = 0
+		num_entries = len(self)
+		while start < num_entries:
+			if self.chunk_size is None:
+				end = num_entries
+			else:
+				end = min(num_entries, start + self.chunk_size)
+
+			yield self.data[start:end]
+			start = end
+
+#### EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF
