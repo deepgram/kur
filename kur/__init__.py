614f1ef636adace311e2cf4cff33aa8e9b1140e8
Adam Sypniewski <ajsyp@syptech.net>
2016-11-23 11:37:02 -0500

    Added package init module.

---
+++ kur/__init__.py
@@ -0,0 +1,32 @@
+"""
+Copyright 2016 Deepgram
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from . import utils
+from . import backend
+from . import containers
+from . import engine
+from . import reader
+from . import loss
+from . import optimizer
+from . import sources
+from . import providers
+from . import model
+from . import supplier
+from . import parsing
+from . import loggers
+from .version import __version__
+
+#### EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF
