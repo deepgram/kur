086d880731b7ce716136e02301773b01599aa8de
Adam Sypniewski <ajsyp@users.noreply.github.com>
2017-04-11 11:35:14 -0400

    Update __main__.py

--- kur/__main__.py
+++ kur/__main__.py
@@ -186,7 +186,8 @@ def prepare_data(args):
 		keys = sorted(batch.keys())
 		num_entries = len(batch[keys[0]])
 		for entry in range(num_entries):
-		        if args.number is None or (args.number < num_entries and entry < args.number) or (entry - num_entries >= args.number):
+			if args.number is None or entry < args.number or \
+				(entry - num_entries >= args.number):
 				print('Entry {}/{}:'.format(entry+1, num_entries))
 				for key in keys:
 					print('  {}: {}'.format(key, batch[key][entry]))
@@ -356,7 +357,8 @@ def parse_args():
 	subparser.add_argument('--assemble', action='store_true', help='Also '
 		'begin assembling the model to pull in compile-time, auxiliary data '
 		'sources.')
-        subparser.add_argument('-n', '--number', type=int, help='number of samples to print')	
+	subparser.add_argument('-n', '--number', type=int,
+		help='Number of samples to print (default: the entire batch).')	
 	subparser.set_defaults(func=prepare_data)
 
 	return parser.parse_args()
