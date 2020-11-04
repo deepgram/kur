fd0c120e50815c1e5be64e5dde964dcd47234556
greedyuser <susan@deepgram.com>
2020-11-04 14:09:50 -0500

    patch for python 3.7 StopIteration change

--- kur/utils/iterhelp.py
+++ kur/utils/iterhelp.py
@@ -17,101 +17,109 @@ limitations under the License.
 from collections import OrderedDict
 from concurrent.futures import ThreadPoolExecutor
 
+
 ###############################################################################
 def get_any_value(x):
-	""" Returns a value of a dict, list, or tuple.
+  """ Returns a value of a dict, list, or tuple.
 
-		# Arguments
+    # Arguments
 
-		x: object. The object to grab a value for.
+    x: object. The object to grab a value for.
 
-		# Return value
+    # Return value
 
-		If `x` is a dictionary (including an OrderedDict), returns a value from
-		it. If `x` is a list or tuple, returns an element from it. Otherwise,
-		raises ValueError.
-	"""
-	if isinstance(x, (dict, OrderedDict)):
-		for v in x.values():
-			return v
-	if isinstance(x, (list, tuple)):
-		return x[0]
-	raise ValueError('Unexpected data type: {}'.format(type(x)))
+    If `x` is a dictionary (including an OrderedDict), returns a value from
+    it. If `x` is a list or tuple, returns an element from it. Otherwise,
+    raises ValueError.
+  """
+  if isinstance(x, (dict, OrderedDict)):
+    for v in x.values():
+      return v
+  if isinstance(x, (list, tuple)):
+    return x[0]
+  raise ValueError('Unexpected data type: {}'.format(type(x)))
+
 
 ###############################################################################
 def get_any_key(x):
-	""" Returns a key from a dictionary.
+  """ Returns a key from a dictionary.
 
-		# Arguments:
+    # Arguments:
 
-		x: dict. The dictionary to pull a key from.
+    x: dict. The dictionary to pull a key from.
 
-		# Return value
+    # Return value
 
-		If x is empty, returns None; otherwise, this returns a key from the
-		dictionary.
-	"""
-	if x:
-		for k in x:
-			return k
-	else:
-		return None
+    If x is empty, returns None; otherwise, this returns a key from the
+    dictionary.
+  """
+  if x:
+    for k in x:
+      return k
+  else:
+    return None
+
 
 ###############################################################################
 def merge_dict(*args):
-	""" Merges any number of dictionaries into a single dictionary.
+  """ Merges any number of dictionaries into a single dictionary.
 
-		# Notes
+    # Notes
 
-		In Python 3.5+, you can just do this:
-		```python
-		r = {**x, **y}
-		```
-		But if you want a single expression in Python 3.4 and below:
-		```python
-		r = merge_dict(x, y)
-		```
-	"""
-	result = {}
-	for x in args:
-		result.update(x)
-	return result
+    In Python 3.5+, you can just do this:
+    ```python
+    r = {**x, **y}
+    ```
+    But if you want a single expression in Python 3.4 and below:
+    ```python
+    r = merge_dict(x, y)
+    ```
+  """
+  result = {}
+  for x in args:
+    result.update(x)
+  return result
+
 
 ###############################################################################
 def partial_sum(x):
-	""" Returns an iterator over the partial sums in a given iterable.
+  """ Returns an iterator over the partial sums in a given iterable.
 
-		# Arguments
+    # Arguments
 
-		x: iterable. The iterable to return partial sums for.
+    x: iterable. The iterable to return partial sums for.
 
-		# Return value
+    # Return value
 
-		At each iteration, yields the most recent partial sum, beginning with
-		`x[0]`.
-	"""
-	result = 0
-	for i in x:
-		result += i
-		yield result
+    At each iteration, yields the most recent partial sum, beginning with
+    `x[0]`.
+  """
+  result = 0
+  for i in x:
+    result += i
+    yield result
+
 
 ###############################################################################
 class parallelize:
 
-	def __init__(self, it):
-		self.it = it
+  def __init__(self, it):
+    self.it = it
 
-	def _next(self, it):
-		return next(it)
+  def _next(self, it):
+    return next(it)
 
-	def __iter__(self):
-		pool = ThreadPoolExecutor(1)
-		it = iter(self.it)
+  def __iter__(self):
+    try:
+      pool = ThreadPoolExecutor(1)
+      it = iter(self.it)
 
-		future = pool.submit(self._next, it)
-		while True:
-			result = future.result()
-			future = pool.submit(self._next, it)
-			yield result
+      future = pool.submit(self._next, it)
+      while True:
+        result = future.result()
+        future = pool.submit(self._next, it)
+        yield result
+    except StopIteration:
+      pass
 
 ### EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF.EOF
